f0f07c296ef0775b9a7a6fb7af43916d8362d8db
kitarira <kitarira_1@icloud.com>
2023-02-08 17:13:02 +0900

    reactバージョンアップと各修正

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client"
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -7,14 +7,16 @@ import { CookiesProvider } from "react-cookie";
 import { Provider } from "react-redux";
 import { store } from "./store"
 
-ReactDOM.render(
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
   <Provider store={store}>
-    <CookiesProvider>
-      <App />
-    </CookiesProvider>
-  </Provider>,
-  document.getElementById("root")
-);
+      <CookiesProvider>
+        <App />
+      </CookiesProvider>
+   </Provider>,
+ );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
